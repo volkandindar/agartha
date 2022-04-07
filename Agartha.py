3dce4aafc8d99588e1aa244287ddcc657526891b
Volkan Dindar <50321735+volkandindar@users.noreply.github.com>
2022-04-07 19:09:18 +0200

    Update Agartha.py

--- Agartha.py
+++ Agartha.py
@@ -737,10 +737,12 @@ class BurpExtender(IBurpExtender, ITab, IMessageEditorController, IContextMenuFa
         self._tbAuthURL.setText(_url)
         self._MainTabs.setSelectedComponent(self._tabAuthSplitpane)
         self._MainTabs.getParent().setSelectedComponent(self._MainTabs)
+        
     def authMatrix(self, ev):
         t = Thread(target=self.authMatrixThread,args=[self])
         t.start()
         return
+
     def _updateReqResView(self, ev):
         try:
             row = self.tableMatrix.getSelectedRow()
