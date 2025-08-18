29688c7c9d6ff28336fa094296321b00c90dcadf
Volkan Dindar <50321735+volkandindar@users.noreply.github.com>
2025-08-18 22:35:48 +0200

    Update Agartha.py

--- Agartha.py
+++ Agartha.py
@@ -26,7 +26,7 @@ except:
     print "==== ERROR ====" + "\n\nFailed to load dependencies.\n" +str(sys.exc_info()[1]) +"\n\n==== ERROR ====\n\n"
     sys.exit(1)
 
-VERSION = "2.36"
+VERSION = "2.37"
 #url_regex = r'(log|sign)([-_+%0-9]{0,5})(off|out|in|on)|(expire|kill|terminat|delete|remove)'
 url_regex = r'(log|sign|time)([-_+%0-9]{0,5})(off|out)|(expire|kill|terminat|delete|remove)'
 ext_regex = r'^\.(gif|jpg|jpeg|png|css|js|ico|svg|eot|woff2|ttf|otf)$'
@@ -1859,7 +1859,7 @@ for (String httpMethod : httpMethods)
         bambdas += "// How many days to process\n"
         bambdas += "if (requestResponse.time().isAfter(ZonedDateTime.now().minusDays(" + self._cbBambdasProcessDays.getSelectedItem().split()[0] + "))){\n"
         
-        if self._cbBambdasValuable.isSelected():
+        if self._cbBambdasValuable.isSelected() and (self._cbBambdasSearchinReq.isSelected() or self._cbBambdasSearchinRes.isSelected() or self._cbBambdasSearchinURL.isSelected()):
             bambdas += "\tList<Pattern> patterns = new ArrayList<>();"
             bambdas += """
     for (String highValueWord : highValueWords)
