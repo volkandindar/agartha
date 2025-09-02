84cb7a430e08b4160c4e22f238e92a0d55c890b7
Volkan Dindar <50321735+volkandindar@users.noreply.github.com>
2025-09-02 15:45:13 +0200

    Update Agartha.py

--- Agartha.py
+++ Agartha.py
@@ -3872,7 +3872,7 @@ if (!suspiciousHit && !matchedScope && !matchedDone)
         htmlString = "<html><body><table width=1000 border=0 cellspacing=0><tr><td><h3>Author:\t\t\tVolkan Dindar<br/>Github:\t\t\thttps://github.com/volkandindar/agartha</h3><br/>"
         htmlString += """
         <h1>Agartha</h1>
-        <h4>Payload Injection (LFI, RCE, SQLi, with optional BCheck), Auth Issues (Access Matrix, HTTP 403), HTTP-to-JavaScript, and Bambdas</h4>
+        <h4>Payload Injection (LFI, RCE, SQLi, with optional BCheck), Auth Issues (Access Matrix, HTTP 403), Copy as JavaScript, and Bambdas</h4>
         <hr/>
         <p>Agartha, specializes in advance payload generation and access control assessment. It adeptly identifies vulnerabilities related to injection attacks, and authentication/authorization issues. The dynamic payload generator crafts extensive wordlists for various injection vectors, including SQL Injection, Local File Inclusion (LFI), and Remote Code Execution(RCE). Furthermore, the extension constructs a comprehensive user access matrix, revealing potential access violations and privilege escalation paths. It also assists in performing HTTP 403 bypass checks, shedding light on auth misconfigurations. Additionally, it can convert HTTP requests to JavaScript code to help digging up XSS issues more.</p>
         <p></p>
