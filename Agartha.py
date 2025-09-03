23288e3166e39b3a2833678de516e56b71cac17a
Volkan Dindar <50321735+volkandindar@users.noreply.github.com>
2025-09-03 23:33:10 +0200

    BApp Store version v3.0

--- Agartha.py
+++ Agartha.py
@@ -4,7 +4,6 @@ Author: Volkan Dindar
         volkan.dindar@owasp.org
         https://github.com/volkandindar/agartha
 """
-
 try:
     import sys, re, urlparse, random, os, urllib, posixpath, json
     from burp import IBurpExtender, ITab, IMessageEditorController, IContextMenuFactory, IBurpExtenderCallbacks, IExtensionHelpers
