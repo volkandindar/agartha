9739a31b75d67f272847c716ff608a20f21a4c2a
Volkan Dindar <50321735+volkandindar@users.noreply.github.com>
2026-02-04 13:29:34 +0100

    Update Agartha.py

--- Agartha.py
+++ Agartha.py
@@ -24,7 +24,7 @@ except:
     print "==== ERROR ====" + "\n\nFailed to load dependencies.\n" +str(sys.exc_info()[1]) +"\n\n==== ERROR ====\n\n"
     sys.exit(1)
 
-VERSION = "3.1006"
+VERSION = "3.1007"
 url_regex = r'(log|sign|time)([-_+%0-9]{0,5})(off|out)|(expire|kill|terminat|delete|remove)'
 ext_regex = r'^\.(gif|jpg|jpeg|png|css|js|ico|svg|eot|woff2|ttf|otf)$'
 
